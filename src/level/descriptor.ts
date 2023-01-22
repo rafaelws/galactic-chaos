9c8d4d501dd33b946624ab6a72d1bb9930443b03
rafaelws <rafael.scheidmandel@gmail.com>
2023-01-22 19:19:39 -0300

    small refac on player + rendering rocks @ firstLevel

--- src/level/descriptor.ts
+++ src/level/descriptor.ts
@@ -1,9 +1,36 @@
 import { assets, getImage } from "@/common/asset";
-import { Player } from "@/objects";
+import { Player, Rock } from "@/objects";
 import { Level } from "./Level";
 
+// TODO break this file into level1/firstStep.ts
+// TODO receive parameters (loaded assets*?) for each step function?
 function firstStep() {
-  return [new Player(getImage(assets.img.player.self))];
+  const rock3 = getImage(assets.img.rock.brown[6]);
+  return [
+    new Player(getImage(assets.img.player.self)),
+    new Rock({
+      img: rock3,
+      start: { x: 0.5, y: 0.5 },
+      angle: -60,
+      delay: 1000,
+      speed: 0.1,
+      rotation: {
+        direction: "COUNTERCLOCKWISE",
+        speed: 5,
+      },
+    }),
+    new Rock({
+      img: rock3,
+      start: { x: 0.5, y: 0 },
+      angle: 60,
+      delay: 2000,
+      speed: 0.2,
+      rotation: {
+        direction: "CLOCKWISE",
+        speed: 2,
+      },
+    }),
+  ];
 }
 
 function secondStep() {
