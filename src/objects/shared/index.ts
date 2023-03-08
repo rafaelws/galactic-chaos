4ad328d7e86b90bd515ce05ec7db84b9f73b6d51
rafaelws <rafael.scheidmandel@gmail.com>
2023-03-08 00:37:35 -0300

    movement index.ts fix

--- src/objects/shared/index.ts
+++ src/objects/shared/index.ts
@@ -3,14 +3,8 @@ export * from "./GameEvent";
 export * from "./impact/ImpactParams";
 export * from "./impact/Impact";
 
-export * from "./movement/MovementParamsLegacy";
-export * from "./movement/MovementLegacy";
-
 export * from "./movement/MovementParams";
 export * from "./movement/Movement";
-
-export * from "./movement/CyclicMovementParams";
-export * from "./movement/CyclicMovement";
 
 export * from "./fire/FireParams";
 export * from "./fire/Fire";
