70b30b0a0635907e4b1c58251671b9d542397dff
rafaelws <rafael.scheidmandel@gmail.com>
2023-03-27 09:15:02 -0300

    WIP: first level-wave two

--- src/level/levels/firstLevel.ts
+++ src/level/levels/firstLevel.ts
@@ -4,7 +4,9 @@ import { AudioManager } from "@/main/AudioManager";
 import { GameObject, healItem, Rock } from "@/objects";
 import { linear } from "@/objects/shared";
 
-function firstWave(rocks: typeof assets.img.rock.brown) {
+type BrownRocks = typeof assets.img.rock.brown;
+
+function firstWave(rocks: BrownRocks) {
   return [
     new Rock({
       spawnTimeout: 3.7 * 1000,
@@ -13,19 +15,13 @@ function firstWave(rocks: typeof assets.img.rock.brown) {
     }),
 
     new Rock({
-      spawnTimeout: 4.9 * 1000,
-      img: getImage(rocks[8]),
-      movement: linear(p(0.5, 0), p(0.5, 1)),
-    }),
-
-    new Rock({
-      spawnTimeout: 5.4 * 1000,
+      spawnTimeout: 5.6 * 1000,
       img: getImage(rocks[7]),
       movement: linear(p(0.85, 0), p(0.15, 1)),
     }),
 
     new Rock({
-      spawnTimeout: 6.5 * 1000,
+      spawnTimeout: 6.45 * 1000,
       img: getImage(rocks[8]),
       movement: linear(p(1, 0.65), p(0, 1)),
     }),
@@ -40,6 +36,40 @@ function firstWave(rocks: typeof assets.img.rock.brown) {
       spawnTimeout: 8.7 * 1000,
       img: getImage(rocks[6]),
       movement: linear(p(1, 0.68), p(0, 0.2)),
+    }),
+  ];
+}
+
+function secondWave(rocks: BrownRocks) {
+  return [
+    new Rock({
+      spawnTimeout: 11.766 * 1000,
+      img: getImage(rocks[9]),
+      movement: linear(p(0.85, 0), p(0.15, 1)),
+    }),
+
+    new Rock({
+      spawnTimeout: 13.633 * 1000,
+      img: getImage(rocks[7]),
+      movement: linear(p(0.15, 0), p(0.85, 1)),
+    }),
+
+    new Rock({
+      spawnTimeout: 14.5 * 1000,
+      img: getImage(rocks[8]),
+      movement: linear(p(0, 0.75), p(1, 1)),
+    }),
+
+    new Rock({
+      spawnTimeout: 15.6 * 1000,
+      img: getImage(rocks[9]),
+      movement: linear(p(1, 0.68), p(0, 0.2)),
+    }),
+
+    new Rock({
+      spawnTimeout: 16.8 * 1000,
+      img: getImage(rocks[6]),
+      movement: linear(p(0, 0.15), p(1, 0.8)),
     }),
   ];
 }
@@ -71,6 +101,7 @@ export async function firstLevel(): Promise<GameObject[]> {
 
   return [
     ...firstWave(rocks),
+    ...secondWave(rocks),
 
     new Rock({
       spawnTimeout: 50 * 1000,
