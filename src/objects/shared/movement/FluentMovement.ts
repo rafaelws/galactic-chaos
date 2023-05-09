63314f0357077fee2ad5d2f3cb5010e5341fb2e7
rafaelws <rafael.scheidmandel@gmail.com>
2023-05-09 12:41:56 -0300

    FluentMovement spacing fix

--- src/objects/shared/movement/FluentMovement.ts
+++ src/objects/shared/movement/FluentMovement.ts
@@ -5,22 +5,11 @@ export function linear(p0: Point, p1: Point, speed?: number) {
   return new FluentMovement().linear(p0, p1, speed);
 }
 
-export function quadraticBezier(
-  p0: Point,
-  p1: Point,
-  p2: Point,
-  speed?: number
-) {
+export function quadraticBezier(p0: Point, p1: Point, p2: Point, speed?: number) {
   return new FluentMovement().quadraticBezier(p0, p1, p2, speed);
 }
 
-export function cubicBezier(
-  p0: Point,
-  p1: Point,
-  p2: Point,
-  p3: Point,
-  speed?: number
-) {
+export function cubicBezier(p0: Point, p1: Point, p2: Point, p3: Point, speed?: number) {
   return new FluentMovement().cubicBezier(p0, p1, p2, p3, speed);
 }
 
@@ -30,41 +19,17 @@ export class FluentMovement {
   };
 
   public linear(p0: Point, p1: Point, speed?: number) {
-    this.params.steps.push({
-      nature: MovementNature.Linear,
-      p0: p0,
-      p1: p1,
-      speed,
-    });
+    this.params.steps.push({ nature: MovementNature.Linear, p0: p0, p1: p1, speed });
     return this;
   }
 
   public quadraticBezier(p0: Point, p1: Point, p2: Point, speed?: number) {
-    this.params.steps.push({
-      nature: MovementNature.QuadraticBezier,
-      p0,
-      p1,
-      p2,
-      speed,
-    });
+    this.params.steps.push({ nature: MovementNature.QuadraticBezier, p0, p1, p2, speed });
     return this;
   }
 
-  public cubicBezier(
-    p0: Point,
-    p1: Point,
-    p2: Point,
-    p3: Point,
-    speed?: number
-  ) {
-    this.params.steps.push({
-      nature: MovementNature.CubicBezier,
-      p0,
-      p1,
-      p2,
-      p3,
-      speed,
-    });
+  public cubicBezier(p0: Point, p1: Point, p2: Point, p3: Point, speed?: number) {
+    this.params.steps.push({ nature: MovementNature.CubicBezier, p0, p1, p2, p3, speed });
     return this;
   }
 
