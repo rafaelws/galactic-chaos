4b4bedbeeb55a4dff0822b84475fad09a5a0ff5e
rafaelws <rafael.scheidmandel@gmail.com>
2023-07-18 00:52:59 -0300

    refac: slider component cleanup

--- src/docs/components/slider/Slider.tsx
+++ src/docs/components/slider/Slider.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from "react";
 
+import { clamp, lerp } from "@/core/math";
 import { Point } from "@/core/meta";
 
 type Orientation = "vertical" | "horizontal";
@@ -13,47 +14,29 @@ interface Props {
   min: number;
   max: number;
   orientation?: Orientation;
+  disabled?: boolean;
 }
 
-// FIXME step
-// FIXME getSliderSize (find a better approach)
+// TODO missing feature: step
+// FIXME value is always floored
 // FIXME after `dragEnd`, `handleSliderClick` is fired
-export function Slider({ onValue, ...props }: Props) {
+export function Slider(props: Props) {
   const sliderRef = useRef<HTMLDivElement>(null);
   const thumbRef = useRef<HTMLSpanElement>(null);
+  const mouseOffsetRef = useRef<number>(0);
+  const rangeRef = useRef<number>(props.max - props.min);
 
-  const range = props.max - props.min;
   const orientation = props.orientation || "horizontal";
   const isHorizontal = orientation === "horizontal";
-  const cssTrackProperty = isHorizontal ? "width" : "height";
+  const cssProperty = isHorizontal ? "left" : "top";
 
-  const getSliderSize = (slider = sliderRef.current) => {
-    if (!slider) return 0;
-    return slider[isHorizontal ? "clientWidth" : "clientHeight"];
-  };
-
-  const toPosition = (val: number, sliderSize = getSliderSize()) => {
-    if (range === 0) return 0;
-    const normalized = Math.max(Math.min(val, props.max), props.min);
-    return ((normalized - props.min) / range) * sliderSize;
-  };
-
-  const toValue = (position: number, sliderSize = getSliderSize()) => {
-    if (sliderSize === 0) return 0;
-    const percent = position / sliderSize;
-    return percent * range + props.min;
-  };
-
-  const [value, setValue] = useState<number>(props.value);
+  const [value, setValue] = useState<number>();
   const [position, setPosition] = useState<number>();
 
   useEffect(() => {
-    setPosition(toPosition(props.value));
-  }, []);
-
-  useEffect(() => {
-    if (value !== undefined) onValue(value);
-  }, [value, onValue]);
+    setValue(props.value);
+    updatePosition(props.value);
+  }, [props.value]);
 
   function drag(ev: globalThis.MouseEvent) {
     moveTo({ x: ev.clientX, y: ev.clientY });
@@ -64,38 +47,60 @@ export function Slider({ onValue, ...props }: Props) {
     window.removeEventListener("mouseup", dragEnd);
   }
 
-  function dragStart() {
+  function dragStart(ev: MouseEvent<HTMLSpanElement>) {
+    calculateThumbOffset({ x: ev.clientX, y: ev.clientY });
     window.addEventListener("mousemove", drag);
     window.addEventListener("mouseup", dragEnd);
   }
 
-  function moveTo(
-    point: Point,
-    slider = sliderRef.current,
-    thumb = thumbRef.current
+  function calculateThumbOffset(
+    { x, y }: Point,
+    thumbRect = thumbRef.current?.getBoundingClientRect()
   ) {
-    if (!slider || !thumb) return;
-    const { width, height, top, left } = slider.getBoundingClientRect();
-
-    // FIXME properly offset
-    const offset = thumb.clientWidth * 0.5;
-
-    const max = isHorizontal ? width : height;
-    let pos = (isHorizontal ? point.x - left : point.y - top) - offset;
-
-    if (pos > max) pos = max;
-    else if (pos < 0) pos = 0;
-
-    setPosition(pos);
-    setValue(Math.floor(toValue(pos)));
+    if (!thumbRect) return;
+    mouseOffsetRef.current = isHorizontal
+      ? x - thumbRect.left
+      : y - thumbRect.top;
   }
 
-  function handleSliderClick(ev: MouseEvent<HTMLDivElement>) {
-    moveTo({ x: ev.clientX, y: ev.clientY });
+  function getMax(
+    sliderRect = sliderRef.current?.getBoundingClientRect(),
+    thumb = thumbRef.current
+  ) {
+    if (!sliderRect || !thumb) return 0;
+    const thumbSize = isHorizontal ? thumb.clientWidth : thumb.clientHeight;
+    return (isHorizontal ? sliderRect.width : sliderRect.height) - thumbSize;
+  }
+
+  function moveTo(
+    { x, y }: Point,
+    sliderRect = sliderRef.current?.getBoundingClientRect(),
+    offset = mouseOffsetRef.current
+  ) {
+    if (props.disabled || !sliderRect) return;
+    const { left, top } = sliderRect;
+    let pos = (isHorizontal ? x - left : y - top) - offset;
+
+    const max = getMax(sliderRect);
+    pos = clamp(pos, 0, max);
+    updateValue(pos / max);
+    setPosition(pos);
+  }
+
+  function updateValue(normalizedPosition: number) {
+    const val = Math.floor(lerp(props.min, props.max, normalizedPosition));
+    setValue(val);
+    props.onValue(val);
+  }
+
+  function updatePosition(val: number, range = rangeRef.current) {
+    if (range === 0) return;
+    const normalized = Math.max(Math.min(val, props.max), props.min);
+    const pos = ((normalized - props.min) / range) * getMax();
+    setPosition(pos);
   }
 
   function handleInput(ev: ChangeEvent<HTMLInputElement>) {
-    ev.preventDefault();
     let val = Number(ev.target.value);
     if (isNaN(val)) {
       val = value || props.value;
@@ -104,16 +109,16 @@ export function Slider({ onValue, ...props }: Props) {
       return setValue(val);
     }
     if (val === value) return;
-    if (val >= props.max) val = props.max;
-    else if (val <= props.min) val = props.min;
-    setValue(val);
-    setPosition(toPosition(val));
+    val = clamp(val, props.min, props.max);
+
     ev.target.value = val.toString();
+    updatePosition(val);
+    setValue(val);
   }
 
   function reset() {
     setValue(props.value);
-    setPosition(toPosition(props.value));
+    updatePosition(props.value);
   }
 
   return (
@@ -130,21 +135,18 @@ export function Slider({ onValue, ...props }: Props) {
       )}
       <div
         ref={sliderRef}
-        className={`slider ${orientation}`}
-        onClick={handleSliderClick}
+        className={`slider ${orientation} ${props.disabled ? "disabled" : ""}`}
+        onClick={(ev) => moveTo({ x: ev.clientX, y: ev.clientY })}
       >
-        <div
-          className="track"
-          style={{
-            [cssTrackProperty]: position,
-          }}
-        ></div>
-        <span
-          className="thumb"
-          ref={thumbRef}
-          onMouseDown={dragStart}
-          onDoubleClick={reset}
-        ></span>
+        <div className="track">
+          <span
+            className="thumb"
+            ref={thumbRef}
+            onMouseDown={dragStart}
+            onDoubleClick={reset}
+            style={{ [cssProperty]: position }}
+          />
+        </div>
       </div>
     </>
   );
